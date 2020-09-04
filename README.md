1bfc2002f5fe3288dd828bff49c865ffd8858c2b
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-09-04 09:43:58 +0800

    first commit with basic file structure

---
+++ README.md
@@ -0,0 +1,28 @@
+
+![small](/public/images/small.png) <br>
+![large](/public/images/large.png) <br>
+
+# FITNESS-TRACKER
+This is a Workout Track mobile first app built with mongoDB. express.js, JavaScript, HTML5 and CSS3. 
+It's a simple CRUD application where you can store your routines after completing them. The app shows your most recent activity on the top.s
+
+# Technologies Used 
+mongoDB, mongoose, express.js, Bootstrap, Moment.js
+# working flow of this program
+     This application is full stack program which is connected to database and utillise the functionality of mongoose. The acceptance criteria for this application is the following:
+     ** Add excercise to the new workout.
+     ** Add new exercise to a new workout plan.
+     ** view the combined weight of multiple exercise on the stat page.
+     `` In the models there is a folder called workoutModels.js which is used to create new schema and declaring the keys types``
+
+    The public folder contains app 
+
+
+# Future Development
+Add some type of data vizualization with user's daily progress;
+Add updates on previous routines
+
+
+# Link to Application
+
+[]
---
+++ server.js
@@ -0,0 +1,79 @@
+//Packages that I am going to need
+
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+//connection to the models folder
+const Workout = require("./models/workoutModel");
+
+const PORT = process.env.PORT || 3007;
+const app = express()
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+// this port used connect mongoose and then heroku as wekk
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wtracker", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`------- App running on port http://localhost:${PORT}`);
+});
+
+//creates a new Workout Object and saves in the db
+
+app.post("/api/workouts", function (req, res) {
+    var workout = new Workout(req.body)
+
+    workout.save(err => {
+        if (err) return res.status(500).send(err);
+        return res.status(200).send(workout);
+    })
+})
+
+app.get("/api/workouts", function (req, res) {
+
+    Workout.find({}).then(function (data, err) {
+        if (err) throw err
+        res.send(data)
+    })
+})
+
+app.get("/api/workouts/:id", function (req, res) {
+
+    Workout.findOne({
+
+
+        _id: req.params.id
+    })
+        .then((doc) => {
+            if (doc) {
+                console.log(doc);
+            } else {
+                console.log("no data exist for this id");
+            }
+
+
+
+        }
+        )
+})
+
+
+
+
+app.put("/api/workouts/", function (req, res) {
+    var query = req.body
+    Workout.update({}, { sort: { name: 1 } }).then(function (data, err) {
+
+        console.log(data)// executes
+        res.send(data)
+        if (err) throw err
+
+    })
+})
+
+
+
+module.exports = app
